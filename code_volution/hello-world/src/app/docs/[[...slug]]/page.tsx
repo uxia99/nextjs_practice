d33cd5b7cceba7d24480b496fa01b50c453f27b7
장유정 <306406j@gmail.com>
2024-02-28 01:01:41 +0900

    code_volution practice Commit

---
+++ code_volution/hello-world/src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,22 @@
+export default function Docs({
+  params, 
+} : {
+  params : {
+    slug : string[];
+  };
+}) {
+  if (params.slug?.length === 2) {
+    return (
+      <h1>
+        Viewing Docs for Feature {params.slug[0]} and concep {params.slug[1]}
+      </h1>
+    );
+  } else if (params.slug?.length === 1) {
+    return (
+      <h1>
+        Viewing Docs for Feature for {params.slug[0]}
+      </h1>
+    )
+  }
+  return <h1>Docs HomePage</h1>
+}
---
+++ code_volution/hello-world/src/app/products/[productId]/page.tsx
@@ -0,0 +1,8 @@
+export default function ProductDetails( {
+  params,
+} : {
+  params : {
+    productId : string };
+}) {
+  return <h1>Details about Product {params.productId}</h1>
+}
